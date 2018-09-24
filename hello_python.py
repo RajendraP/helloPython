ebdf1c42f6905b42d73b40fd924a82ac87fe5712
Rajendra Pandey <rupandey@gmail.com>
2018-09-24 18:49:12 +0530

    changed to python3

--- hello_python.py
+++ hello_python.py
@@ -4,6 +4,7 @@ The mod module
 @date: Jan 31, 2016
 """
 
+
 class HelloPython(object):
     """
     hello world in python
@@ -11,26 +12,29 @@ class HelloPython(object):
     def __init__(self, user_name=None, user_age=None):
         self.user_name = user_name
         self.user_age = user_age
+
     def hello_user(self):
         """
-    	A method defination, always called with instance, requires to pass self.
-    	"""
-        print self.user_name
+        A method defination, always called with instance, requires to pass self.
+        """
+        print(self.user_name)
+
     def hello_python(self):
         """
         A method defination, always called with instance, requires to pass self.
         """
-        print self.user_name
+        print(self.user_name)
+
 
 def hello_python():
     """
-    A function defination, doesn't require instance to call.
+    A function definition, doesn't require instance to call.
     """
-    print 'Hello Python!!'
+    print('Hello Python!!')
 
 
-HELLOPYTHON = HelloPython('Peter')
-HELLOPYTHON.hello_user()
+HELLO_PYTHON = HelloPython('Peter')
+HELLO_PYTHON.hello_user()
 
 hello_python()
 
